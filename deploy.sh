bd1babe1027e9409280f9d53c9e952a2c5be6d8a
arsenyan.d <arsenyan.d@office.lan>
2023-12-19 19:45:20 +0400

    add project

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env sh
-# abort on errors
 set -e
-# build
 yarn run build
-# navigate into the build output directory
 cd dist
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com/Sayadh/credit-calculator.git master:gh-pages
+git push -f https://github.com/Sayadh/credit-calculator.git master:gh-pages
 cd -
